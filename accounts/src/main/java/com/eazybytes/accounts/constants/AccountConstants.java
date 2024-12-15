6ef5bc2cf3816340c7bd8636c275978373178095
AbenezerM <abenezermulugeta41@gmail.com>
2024-12-15 00:24:29 -0500

    Add constants

---
+++ accounts/src/main/java/com/eazybytes/accounts/constants/AccountConstants.java
@@ -0,0 +1,18 @@
+package com.eazybytes.accounts.constants;
+
+import lombok.NoArgsConstructor;
+
+public class AccountConstants {
+
+    // this class has static members, so no need to instantiate
+    private AccountConstants(){}
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "123 Main St, New York City, NY";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "An error occurred. Please try again later.";
+}
